8e8e8e99077ef6d721d028d752cd4f4ac208a432
Saosznik <kahockijandrzej@gmail.com>
2021-12-06 08:44:05 +0300

    .

--- Lab_2/Composite/CompositeElements/Sentence.cs
+++ Lab_2/Composite/CompositeElements/Sentence.cs
@@ -79,41 +79,5 @@ namespace Lab_2.Composite.CompositeElements
             foreach (var item in words) tmp += $" {item}";
             return tmp;
         }
-
-        //protected Sentence(string data) : base(data) { }
-        //public override void Parse()
-        //{
-        //    foreach (var text in _texts)
-        //    {
-        //        int ltemp = 0, rtemp = 0;
-        //        foreach (var ch in text.Data)
-        //        {
-        //            if (ch == '!' || ch == '?' || ch == '.')
-        //            {
-        //                if (rtemp == 0 && ltemp == 0) ltemp = text.Data.IndexOf(ch);
-        //                else if (rtemp == 0 && ltemp != 0) rtemp = text.Data.IndexOf(ch);
-        //                _sentences.Add(new Sentence(string.Join(" ", ltemp, rtemp - ltemp)));
-        //            }
-        //        }
-        //    }
-        //}
-        //public override void Parse(Component component)
-        //{
-        //    if ((component as Text) != null)
-        //    {
-        //        int ltemp = 0, rtemp = 0;
-        //        foreach (var ch in ((Text)component).Data)
-        //        {
-        //            if (ch == '!' || ch == '?' || ch == '.')
-        //            {
-        //                if (rtemp == 0 && ltemp == 0) ltemp = ((Text)component).Data.IndexOf(ch);
-        //                else if (rtemp == 0 && ltemp != 0) rtemp = ((Text)component).Data.IndexOf(ch);
-        //                _sentences.Add(new Sentence(string.Join(" ", ltemp, rtemp - ltemp)));
-        //            }
-        //        }
-        //    }
-        //}
-
-        //public override void Remove(Component component) { if((component as Word) != null) _words.Remove((Word)component); }
     }
 }
